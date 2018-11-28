d35ffb2cf7a542521c37dc736c6cfd6a801045d2
prasune <prasunejohn@gmail.com>
2018-11-28 23:21:03 +0530

    added code for publishing kafka messages

--- demo/src/main/java/com/example/demo/rest/controller/UserRestController.java
+++ demo/src/main/java/com/example/demo/rest/controller/UserRestController.java
@@ -5,17 +5,26 @@ import java.util.List;
 
 import javax.ws.rs.core.MediaType;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.rest.model.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/demo/user")
 public class UserRestController {
 
+	private static final String USER_TOPIC = "topic.notif.user";
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+	
 	@RequestMapping(method = RequestMethod.GET, 
 			path="{id}", produces = MediaType.APPLICATION_JSON)
 	public User getUser(
@@ -38,17 +47,12 @@ public class UserRestController {
 
 	@RequestMapping(method = RequestMethod.POST, 
 			produces = MediaType.APPLICATION_JSON, consumes = MediaType.APPLICATION_JSON)
-	public User createUser() {
-		User user = new User();
-		user.setId("1");
-		user.setName("prasune");
-		user.setType("Admin");
-		List<String> roles = new ArrayList<>();
-		roles.add("VIEW");
-		roles.add("MANAGE");
-		user.setRoles(roles);
+	public User createUser(@RequestBody User user) throws JsonProcessingException {		
 		// store user to DB
+		
 		// publish a kafka message for notifying other micro-services
+		ObjectMapper mapper = new ObjectMapper();
+		kafkaTemplate.send(USER_TOPIC, user.getId(), mapper.writeValueAsString(user));
 		return user;
 	}
 }
